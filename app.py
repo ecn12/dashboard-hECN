4482827315b8cc14b2b15b4cc161491e602c65f0
ecn12 <eduardocnovo@gmail.com>
2026-06-01 02:06:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -23,28 +23,72 @@ if arquivo:
         encoding="latin1"
     )
 
-    col1, col2, col3 = st.columns(3)
+    df["datetime"] = pd.to_datetime(
+    df.iloc[:,1],
+    format="%d/%m/%Y %H:%M"
+)
 
-    with col1:
-        st.metric(
-            "Estações",
-            "1"
-        )
+ultima_data = df["datetime"].max()
 
-    with col2:
-        st.metric(
-            "Registros",
-            f"{len(df):,}"
-        )
+dias_dados = (
+    ultima_data.date()
+    -
+    df["datetime"].min().date()
+).days
 
-    with col3:
-        st.metric(
-            "Arquivo",
-            "Carregado"
-        )
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.metric(
+        "Estações",
+        "1"
+    )
+
+with col2:
+    st.metric(
+        "Última Atualização",
+        ultima_data.strftime("%d/%m/%Y")
+    )
+
+with col3:
+    st.metric(
+        "Dias de Dados",
+        f"{dias_dados:,}"
+    )
 
     st.success("CSV carregado com sucesso.")
 
     st.markdown("---")
+    col_mapa, col_card = st.columns([2,1])
+
+    with col_mapa:
+
+    mapa = pd.DataFrame({
+        "lat": [-22.520277],
+        "lon": [-42.830555]
+    })
+
+    st.subheader("Localização da Estação")
+
+    st.map(mapa)
+
+    with col_card:
+
+    st.subheader("Quizanga")
+
+    st.metric(
+        "Nível Atual",
+        "1,37 m"
+    )
+
+    st.metric(
+        "Variação (7 dias)",
+        "-0,20 m"
+    )
+
+    st.metric(
+        "Tendência",
+        "↓ Decrescente"
+    )
 
     st.button("Abrir Estação Quizanga")
