47c86640a1419e8bf8e6207fddc1e21b96ca0ed9
ecn12 <eduardocnovo@gmail.com>
2026-06-27 00:46:45 -0300

    Update Dashboard_V2.py

--- pages/Dashboard_V2.py
+++ pages/Dashboard_V2.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import pandas as pd
 
+from utils.processamento import processar_dados
+import streamlit as st
+import pandas as pd
+
 st.set_page_config(
     page_title="Dashboard Hidrológico",
     layout="wide"
