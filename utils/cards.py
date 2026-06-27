9f33fa2324e07738108ac00fe0b28839f2235d13
ecn12 <eduardocnovo@gmail.com>
2026-06-27 01:32:23 -0300

    Update cards.py

--- utils/cards.py
+++ utils/cards.py
@@ -1,1 +1,16 @@
 
+import streamlit as st
+
+def mostrar_card(
+    nivel_atual,
+    percentil_sazonal,
+    percentil_serie,
+    variacao_m,
+    variacao_pct,
+    tendencia
+):
+
+    st.metric(
+        "Nível Atual",
+        f"{nivel_atual:.2f} m"
+    )
