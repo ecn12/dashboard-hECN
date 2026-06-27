0aaef856b6192aec606427e0267ae997251e0268
ecn12 <eduardocnovo@gmail.com>
2026-06-27 01:08:41 -0300

    Update graficos.py

--- utils/graficos.py
+++ utils/graficos.py
@@ -1,1 +1,13 @@
+def configurar_eixo_x(fig, periodo):
 
+    if periodo in ['15 dias', '1 mês']:
+        fig.update_xaxes(tickformat="%d/%m", dtick="D2")
+
+    elif periodo == '4 meses':
+        fig.update_xaxes(tickformat="%b/%Y", dtick="M1")
+
+    elif periodo == '12 meses':
+        fig.update_xaxes(tickformat="%b/%Y", dtick="M2")
+
+    elif periodo == 'Série completa':
+        fig.update_xaxes(tickformat="%Y", dtick="M12")
