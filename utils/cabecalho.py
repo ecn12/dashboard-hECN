30570214d5bfdd02f5d1a6041b7a0d044905ed51
ecn12 <eduardocnovo@gmail.com>
2026-06-28 20:38:55 -0300

    Update cabecalho.py

--- utils/cabecalho.py
+++ utils/cabecalho.py
@@ -1,1 +1,32 @@
+import streamlit as st
 
+
+def mostrar_cabecalho(
+    rio,
+    estacao,
+    operador,
+    municipio,
+    estado,
+    redec,
+    tipo,
+    ultima_atualizacao
+):
+
+    st.markdown(
+        f"""
+# HECN – Plataforma Hidrológica
+
+## {rio.upper()}
+
+### Estação {estacao} — Operador: {operador}
+
+📍 {municipio} • {estado} • REDEC {redec}
+
+📡 {tipo}
+
+🕒 Atualizado em {ultima_atualizacao.strftime('%d/%m/%Y às %H:%M')}
+
+---
+""",
+        unsafe_allow_html=True
+    )
